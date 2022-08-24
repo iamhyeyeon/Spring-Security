678b07b43b6cf7bf81a598ebe954341dd857b43a
devpooh <huhu708@naver.com>
2022-08-24 16:27:35 +0900

    .

--- demo/src/main/java/com/example/demo/basicsecurity/SecurityController.java
+++ demo/src/main/java/com/example/demo/basicsecurity/SecurityController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.basicsecurity;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
     @GetMapping("/")
     public String index(){
-        return "home";
+        return "home"; //html 출력 내용
     }
 }
