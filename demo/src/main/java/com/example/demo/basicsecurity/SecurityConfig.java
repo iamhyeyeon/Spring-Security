bb16adebc07e6e1d0e9fa21a5de9d710e4511738
devpoooh <huhu708@naver.com>
2022-08-28 20:53:28 +0900

    remeber-me Filter

--- demo/src/main/java/com/example/demo/basicsecurity/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/basicsecurity/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.demo.basicsecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -20,8 +22,12 @@ import java.io.IOException;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserDetailsService userDetailsService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
             .authorizeHttpRequests()
             .anyRequest().authenticated();
@@ -60,7 +66,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     session.invalidate();
                 }
             })
-            .deleteCookies("remeber-me");
+            .deleteCookies("remeber");
 
+        //remember-Me Filter
+            http
+                .rememberMe()
+                .rememberMeParameter("remember") //쿠키 파라미터명: defualt는 remember-me
+                .tokenValiditySeconds(3600) //1시간
+                .userDetailsService(userDetailsService);
     }
 }
